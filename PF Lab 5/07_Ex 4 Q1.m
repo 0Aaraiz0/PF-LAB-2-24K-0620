1e5eaccb7b12bdd90014dd38baa10a729400e4db
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-09-28 17:59:34 +0500

    Update 07_Ex 4 Q1.m

--- PF Lab 5/07_Ex 4 Q1.m
+++ PF Lab 5/07_Ex 4 Q1.m
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main(){
-	int a, b;         
-	printf("Enter a : ");
-	scanf("%d", &a);
-	printf("Enter b : ");
-	scanf("%d", &b);
-a=a^b;
-b=a^b;
-a=a^b;
+int main() {
+    int a, b;         
+    
+    printf("Enter a : ");
+    scanf("%d", &a);
+    
+    printf("Enter b : ");
+    scanf("%d", &b);
 
-printf("a after swap is %d\n", a);
-printf("b after swap is %d", b);
-return 0;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    printf("a after swap is %d\n", a);
+    printf("b after swap is %d\n", b);
+
+    return 0;
 }
