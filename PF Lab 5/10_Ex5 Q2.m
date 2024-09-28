1892e95b2d1ee4f1c27c946c2e15fb337d254ec1
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-09-28 18:03:54 +0500

    Update and rename Question2.txt to 10_Ex5 Q2.m

---
+++ PF Lab 5/10_Ex5 Q2.m
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, sum = 0;
+    
+    printf("Write a number: ");
+    scanf("%d", &a);
+
+    while (a > 0) {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+
+    printf("%d is your sum\n", sum);
+    
+    return 0;
+}
