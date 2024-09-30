a03e7415354118b92b89b0cf6fe643832435df81
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-09-30 08:41:50 +0500

    Create Question 2.c

---
+++ PF Lab 6/Question 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("Enter a Num : ");
+scanf("%d", & n);
+while (n >= 10 )
+{
+    printf("%d is a multiple digit number\n", n);
+    printf("Enter a Num : ");
+    scanf("%d", & n);
+}
+printf("%d is not a multiple digit number", n);
+
+
+return 0;
+}
