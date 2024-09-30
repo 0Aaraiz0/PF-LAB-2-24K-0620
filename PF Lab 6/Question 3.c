17cac301272f955fce29f89927cfd8eb5a9a342a
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-09-30 10:09:07 +0500

    Create Question 3.c

---
+++ PF Lab 6/Question 3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int n , a , b = n/2 ;
+printf("Enter a number : ");
+scanf("%d", &n);
+
+while(n > 0)
+     while(b > 0)
+{
+a = n % b;
+b = b - 1;
+if (a = 0)
+printf("%d is composite", n);
+else printf("%d is prime", n);
+{     
+     
+     printf("Enter a number : ");
+     scanf("%d", &n);
+}
+     printf("Come on man 0");
+return 0;
+}
