1c5f3799fb391d726788bb11b45b2462a9f0e037
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-09-30 10:29:00 +0500

    Update Question 6.c

--- PF Lab 6/Question 6.c
+++ PF Lab 6/Question 6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
+    printf("1 ");
 int n = 0 , sum = 0;
 
 for( int a = 1 , b = 2 ; n < 7 ; n = n + 1)
@@ -7,7 +8,7 @@ for( int a = 1 , b = 2 ; n < 7 ; n = n + 1)
     sum = a * b;
     a = b;
     b = sum;
-printf("%d\n" , sum);
+printf("%d " , sum);
 }
 return 0;
 }
