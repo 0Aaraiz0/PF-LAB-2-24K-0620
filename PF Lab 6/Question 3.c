894cd561d396f3591127f6796f860815c9efce53
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-10-05 17:33:30 +0500

    Update Question 3.c

--- PF Lab 6/Question 3.c
+++ PF Lab 6/Question 3.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
-int main(){
-int n , a , b = n/2 ;
-printf("Enter a number : ");
-scanf("%d", &n);
+#include <math.h>
 
-while(n > 0)
-     while(b > 0)
-{
-a = n % b;
-b = b - 1;
-if (a = 0)
-printf("%d is composite", n);
-else printf("%d is prime", n);
-{     
-     
-     printf("Enter a number : ");
-     scanf("%d", &n);
-}
-     printf("Come on man 0");
-return 0;
+int main() {
+    int n, a, b;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("%d is neither prime nor composite\n", n);
+        return 0;
+    }
+
+    b = sqrt(n);
+    int P = 0;
+    int i = 2;
+
+    while (i <= b) {
+        a = n % i;
+        if (a == 0) {
+            P = 1;
+            break;
+        }
+        i++;
+    }
+
+    if (P == 1) {
+        printf("%d is composite\n", n);
+    } else {
+        printf("%d is prime\n", n);
+    }
+
+    return 0;
 }
