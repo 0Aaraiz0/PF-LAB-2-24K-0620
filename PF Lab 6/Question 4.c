bba5a9cf4e3e4c20ae4c56d2d5f46a7573527aab
Aaraiz Nadeem <aaraizzad40@gmail.com>
2024-10-06 10:48:08 +0500

    Update Question 4.c

--- PF Lab 6/Question 4.c
+++ PF Lab 6/Question 4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(){
-int n = 0 , sum = 0;
+int n = 0 , c = 0;
 
 for( int a = 1, b = 1 ; n <= 6 ; n = n + 1)
 {
     a = b;
-    b = sum;
-    sum = a + b;
+    b = c;
+    c = a + b;
 printf("%d " , sum);
 }
 return 0;
